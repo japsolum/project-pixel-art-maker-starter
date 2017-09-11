da2a754b84f7332b4fea335d8fdded1cb4f7498f
japsolum <japsolum@gmail.com>
2017-09-11 07:15:02 -0600

    Add
    Added function of collecting values from number boxes and started loop to build grid

--- designs.js
+++ designs.js
@@ -3,8 +3,33 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+var width,
+	height;
 
-// Your code goes here!
+$(document).ready(function() {
+	$("#input_height").change(function () {
+		height = $("#input_height").val();
+		$("#input_height").attr("value", height);
+	});
 
-}
+	$("#input_width").change(function () {
+		width = $("#input_width").val();
+		$("#input_width").attr("value", width);
+	});
+});
+
+$("#submit").click(function() {
+	height = $("#input_height").attr("value");
+	width = $("#input_width").attr("value");
+	makeGrid(height, width);
+});
+
+var makeGrid = function(height, width) {	
+	for (var y = 0; y < height.length; y++) {
+		//create row
+		for (var x = 0; x < width.length; x++) {
+			//create all columns in row
+		}
+	}
+	console.log("height " + height + " width " + width);
+};
